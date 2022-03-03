ad7dc9e8c350f84afe1b96f7a6e702e5fdd40ead
Alex Wardle <61836432+XND3000@users.noreply.github.com>
2022-03-03 19:08:16 +0000

    MetaMask Authentication, Toast Notification,NFT PG
    Completion of Metamask Authentication, Inclusion of toaster notifications welcoming back the user to the application once signed in with Metamask. Beginning of NFT Collection page.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero' 
 import {useWeb3} from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
 
 const style = {
     wrapper: ``,
@@ -13,20 +16,58 @@ const style = {
 
 export default function Home(){
     const { address, connectWallet} = useWeb3()
+
+    const welcomeUser = (userName, toastHandler = toast) => {
+        toastHandler.success(
+            `Welcome back${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
+            {
+                style: {
+                    background: '#04111d',
+                    color: '#fff',
+                  },
+                }
+        )    
+    }
+
+    useEffect(() => {
+        if (!address) return
+        ;(async () => {
+          const userDoc = {
+            _type: 'users',
+            _id: address,
+            userName: 'Unnamed',
+            walletAddress: address,
+          }
+    
+          const result = await client.createIfNotExists(userDoc)
+    
+          welcomeUser(result.userName)
+        })()
+      }, [address])
+
     return (
         <div className={style.wrapper}>
+            <Toaster position="top-center" reverseOrder={false}/>
             {address ? (
     <>
      <Header />  
      <Hero />
     </>
             ) : (
-            <button
+            
+             <div className={style.walletConnectWrapper}>
+                <button
                 className={style.button}
                 onClick={() => connectWallet('injected')}
                 >
                     Connect Wallet
                 </button>
+                <div className={style.details}>
+                    You need Chrome to be
+                <br/> able to run this app.
+                    </div>
+                </div>
+                
         )}
      </div>
     )
